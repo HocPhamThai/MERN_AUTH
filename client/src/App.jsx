e6d358265709a8ef11c76399dc376f9dfa2e1ae7
Pham Thai Hoc <89366106+HocerChicken@users.noreply.github.com>
2024-04-23 22:08:05 +0700

    Hello MERN_AUTH

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
